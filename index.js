3faa3e687eb8b74cb6924e6f77d7391faabe65cb
Jason <jason@Jasons-MacBook-Air.local>
2023-05-11 15:50:02 -0700

    addeventListener

--- index.js
+++ index.js
@@ -6,21 +6,40 @@ let direction = null;
 let x = 100;
 let y = 250;
 
-if(direction === 'west'){
-    x = x - 1
-}
-if(direction === 'north'){
-    y = y + 1
-}
-if(direction === 'east'){
-    x = x + 1
-}
-if(direction === 'south'){
-    y = y - 1
-}
+setInterval(function(){ 
+    if(direction === 'west'){
+        x = x - 1
+    }
+    if(direction === 'north'){
+        y = y + 1
+    }
+    if(direction === 'east'){
+        x = x + 1
+    }
+    if(direction === 'south'){
+        y = y - 1
+    }
+    character.style.left = x + 'px'
+    character.style.bottom = y + 'px'
+}, 1)
 
-character.style.left = x + 'px'
-character.style.bottom = y + 'px'
+document.addEventListener('keydown', function(e){
+    if(e.repeat) return;
+
+    if(e.key === 'ArrowLeft'){
+        direction = 'west'
+    }
+    if(e.key === 'ArrowUp'){
+        direction = 'north'
+    }
+    if(e.key === 'ArrowRight'){
+        direction = 'east'
+    }
+    if(e.key === 'ArrowDown'){
+        direction = 'south'
+    }
+})
+
 
 move(character).to(100, 250)
 
